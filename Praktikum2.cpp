b01ead1e641267847535061a1335bf8058b684e6
Razfar14 <rajarangkuty14@gmail.com>
2026-03-03 15:34:53 +0700

    mengimplementasiProsedur&Fungsi

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -4,4 +4,22 @@ using namespace std;
 //Deklarasi variabel global
 float p,l;
 
+//Implementasi prosedur&funngsi
+void input(){
+    cout << "Masukkan panjang: ";
+    cin >> p;
+    cout << "Masukkan lebar: ";
+    cin >> l;
+}
+
+float luasPersegi(float x, float y){
+    return x*y;
+}
+int jumlah(int a, int b, int c){
+    return a+b+c;
+}
+
+void output(){
+    cout << "Luas persegi: " << luasPersegi(p,l) << endl;
+ }
 
