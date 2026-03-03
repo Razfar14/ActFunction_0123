eb897eb3e0523aeb78ab8ec4c78693c42a048801
Razfar14 <rajarangkuty14@gmail.com>
2026-03-03 15:25:46 +0700

    membuatProsedurInput

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+//Deklarasi variabel global
+    float p, l;
 
+//Implementasi prosedur&funngsi
+void input(){
+    cout << "Masukkan panjang: ";
+    cin >> p;
+    cout << "Masukkan tinggi: ";
+    cin >> l;
+}
 
 
